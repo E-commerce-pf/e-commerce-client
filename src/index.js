b252f8c4b89903cdf07697f8e6e293e971ccc1a9
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-15 10:36:32 -0500

    Axios Config deploy

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
 
 import store from "./Redux/Store";
 import "./index.css";
 import App from "./App";
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const root = document.getElementById("root");
 
