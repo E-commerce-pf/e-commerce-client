2494ca3a0dd01c7432edb02d2af4e0500a91165c
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-15 10:16:26 -0500

    Config Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import store from "./Redux/Store";
 import "./index.css";
 import App from "./App";
 
 const root = document.getElementById("root");
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  root
+);
