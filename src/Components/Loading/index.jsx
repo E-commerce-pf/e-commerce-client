8b8202aa456d7d42ef61dec473bcc71f068f4aca
Jose Angarita <amjose09@gmail.com>
2022-03-16 17:42:13 -0500

    todo terminado

--- src/Components/Loading/index.jsx
+++ src/Components/Loading/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import styled from "./Loading.module.css";
-import Load from "../../Assets/Gifs/loading.gif";
+import Load from "../../Assets/Gifts/loading (1).gif";
 
 const Loading = () => (
   <div className={styled.container}>
