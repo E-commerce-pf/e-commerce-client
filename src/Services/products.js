a51bba9ebbd4d7919caaeb6b429d194a014fe4e4
David De la Hoz <dricardelo@gmail.com>
2022-03-16 22:41:46 -0500

    create product

--- src/Services/products.js
+++ src/Services/products.js
@@ -4,6 +4,11 @@ const getAllProducts = async () => {
   return (await axios.get("/products")).data;
 };
 
+//falta crear ruta en el back
+const createProduct = async (product) => {
+  await axios.post("/product", product);
+};
+
 const productsService = {
   getAllProducts,
 };
