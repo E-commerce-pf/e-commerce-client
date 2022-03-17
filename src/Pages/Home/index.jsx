9e9645f5b4520bedf0c62dd9b77aa6cc1830c827
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-16 22:12:50 -0500

    Footer

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import productsService from "../../Services/products";
 import { setAllProducts } from "../../Redux/Actions/productsActions";
-import imgHome2 from '../../Assets/Images/imgHome2jpg.jpg'
+import imgHome2 from "../../Assets/Images/imgHome2jpg.jpg";
 import Loading from "../../Components/Loading";
-import {CardProduct} from "../../Components/CardProduct";
-import { Footer } from "../../Components/footer/Footer";
-import './Home.css'
+import { CardProduct } from "../../Components/CardProduct";
+import Footer from "../../Components/Footer";
+import "./Home.css";
 import { Navbar } from "../../Components/Navbar/Navbar";
 
 export const Home = () => {
@@ -25,24 +25,28 @@ export const Home = () => {
 
   return (
     <div className="container_home">
-      <Navbar/>
+      <Navbar />
       <div className="container-info-3">
-        <img src={imgHome2} alt="imagen" width='100%' height='250px' />
+        <img src={imgHome2} alt="imagen" width="100%" height="250px" />
       </div>
-      <div className='container-info-4'>
-        <div className='nuevo'>
+      <div className="container-info-4">
+        <div className="nuevo">
           <h2>Nuevo en </h2>
         </div>
-        <div className='card'>
-        {
-          products?.map(el=>(
-            <CardProduct key={el.id}img={el.image} title={el.title} id={el.id}price={el.price} category={el.category}/>
-          ))
-        }
+        <div className="card">
+          {products?.map((el) => (
+            <CardProduct
+              key={el.id}
+              img={el.image}
+              title={el.title}
+              id={el.id}
+              price={el.price}
+              category={el.category}
+            />
+          ))}
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
-
