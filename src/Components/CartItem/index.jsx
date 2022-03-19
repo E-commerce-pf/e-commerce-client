3b4b762c6c97e07baf93ebd8020b0c002675f257
carlafaes <carlafaes18@hotmail.com>
2022-03-18 19:34:23 -0600

    estilos paginado

--- src/Components/CartItem/index.jsx
+++ src/Components/CartItem/index.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import AddToBag from "../AddToBag";
 import RemoveToBag from "../RemoveToBag";
 import styled from "./CartItem.module.scss";
+import { FaTrashAlt } from "react-icons/fa";
 
 const CartItem = ({ title, price, id, amount }) => {
   return (
     <div className={styled.container}>
-      <button className={styled.btn_quitar}>Quitar del carrito</button>
-      <p className={styled.text_shp}>Nombre: {title}</p>
+      <button className={styled.btn_quitar}><FaTrashAlt/>Quitar del carrito</button>
+      <p className={styled.text_shp}>{title}</p>
       <div className={styled.price}>
         <p className={styled.text_shp}>Precio: {price}$</p>        
         <p className={styled.text_shp}>Total: {price * amount}$</p>
--- src/Components/CartShoppingBag/index.jsx
+++ src/Components/CartShoppingBag/index.jsx
@@ -1,11 +1,12 @@
 import CardItem from "../CartItem";
 import styled from "./CartShoppingBag.module.css";
+import { FaRegFrownOpen } from "react-icons/fa";
 
 const CartShoppingBag = ({ cartItems }) => {
   return (
     <aside className={styled.container}>
       <h1 className={styled.text_shop}>SHOPPING BAG</h1>
-      {cartItems.length === 0 ? <p className={styled.text_shop}>Aun no tienes productos en el carrito</p> : null}
+      {cartItems.length === 0 ? <div className={styled.text_shop}>Aun no tienes productos en el carrito<FaRegFrownOpen/></div> : null}
       {cartItems.map((item) => (
         <CardItem key={item.id} {...item} />
       ))}
