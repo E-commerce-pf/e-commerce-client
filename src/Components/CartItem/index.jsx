22eaaad656d93bd975af03ad8ea96d7bf8c6dc57
carlafaes <carlafaes18@hotmail.com>
2022-03-19 11:23:20 -0600

    f

--- src/Components/CartItem/index.jsx
+++ src/Components/CartItem/index.jsx
@@ -16,7 +16,7 @@ const CartItem = ({ title, price, id, amount }) => {
       </div>
       <div className={styled.button}>
         <RemoveToBag text={"-"} id={id} />
-        <p>{amount}</p>
+        <p>{amount}f</p>
         <AddToBag text={"+"} id={id} />
       </div>
     </div>
