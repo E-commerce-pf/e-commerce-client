0bd3dd259b5a2b6fe71ef188feb721e0aec76e55
BuildTools <unconfigured@null.spigotmc.org>
2022-03-20 21:28:11 -0300

    fix

--- src/Utils/paginate.js
+++ src/Utils/paginate.js
@@ -2,6 +2,5 @@ export const Paginate = (arr, page, elemPage) => {
   let numPages = Math.ceil(arr.length / elemPage);
   page = page < 0 ? page * -1 : page;
   page = page % numPages;
-  console.log(arr)
-  return arr.products?.slice(elemPage * page, elemPage * page + elemPage);
+  return arr?.slice(elemPage * page, elemPage * page + elemPage);
 };
