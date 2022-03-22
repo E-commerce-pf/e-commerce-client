220259421923d625604031821ae0ef350ccde300
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2022-03-22 12:10:48 -0500

    Bugs arreglados

--- src/Components/CardProduct/index.jsx
+++ src/Components/CardProduct/index.jsx
@@ -1,106 +1,106 @@
-import React from 'react';
-import AddToBag from '../AddToBag';
-import Link from '@material-ui/core/Link';
-import Rating from '@mui/material/Rating';
-import { withStyles } from '@material-ui/core/styles';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Button, CardActionArea } from '@mui/material';
+import React from "react";
+import AddToBag from "../AddToBag";
+import Link from "@material-ui/core/Link";
+import Rating from "@mui/material/Rating";
+import { withStyles } from "@material-ui/core/styles";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Button, CardActionArea } from "@mui/material";
 const ExpandMore = styled((props) => {
-	const { expand, ...other } = props;
-	return <IconButton {...other} />;
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-	transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-	marginLeft: 'auto',
-	transition: theme.transitions.create('transform', {
-		duration: theme.transitions.duration.shortest,
-	}),
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
 }));
 
 const CardProduct = ({
-	image,
-	title,
-	id,
-	price,
-	stock,
-	discount,
-	score,
-	description,
+  image,
+  title,
+  id,
+  price,
+  stock,
+  discount,
+  score,
+  description,
 }) => {
-	const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = React.useState(false);
 
-	const handleExpandClick = () => {
-		setExpanded(!expanded);
-	};
-	return (
-		<>
-			<Card sx={{ maxWidth: 345 }}>
-				<CardActionArea>
-					<CardHeader title={title} subheader={`PRECIO: ${price}$`} />
-					<CardMedia
-						component='img'
-						height='194'
-						image={image}
-						alt='Paella dish'
-					/>
-					<CardContent>
-						<Typography variant='body6' color='text.secondary'>
-							{`STOCK: ${stock}`} {`DESCUENTO: ${discount}`}
-						</Typography>{' '}
-						<Typography variant='body2' color='textSecondary' component='p'>
-							{score && <Rating name='read-only' value={score} readOnly />}
-						</Typography>
-					</CardContent>
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  return (
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardHeader title={title} subheader={`PRECIO: ${price}$`} />
+          <CardMedia
+            component="img"
+            height="194"
+            image={image}
+            alt="Paella dish"
+          />
+          <CardContent>
+            <Typography variant="body6" color="text.secondary">
+              {`STOCK: ${stock}`} {`DESCUENTO: ${discount * 100} %`}
+            </Typography>{" "}
+            <Typography variant="body2" color="textSecondary" component="p">
+              {score && <Rating name="read-only" value={score} readOnly />}
+            </Typography>
+          </CardContent>
 
-					<CardActions disableSpacing>
-						<IconButton aria-label='add to favorites'>
-							<AddToBag text={'Añadir al carrito'} id={id} />
-						</IconButton>
-						<IconButton aria-label='share'>
-							<FavoriteIcon />
-						</IconButton>
-						<ExpandMore
-							expand={expanded}
-							onClick={handleExpandClick}
-							aria-expanded={expanded}
-							aria-label='show more'
-						>
-							<ExpandMoreIcon />
-						</ExpandMore>
-					</CardActions>
-				</CardActionArea>
-				<Collapse in={expanded} timeout='auto' unmountOnExit>
-					<CardContent>
-						<Typography paragraph>información adicional</Typography>
-						<Typography paragraph>{description}</Typography>
-						<div>
-							<Button
-								variant='contained'
-								color='primary'
-								Link
-								component={Link}
-								href={`/product/${id}`}
-							>
-								Ver más
-							</Button>
-						</div>
-					</CardContent>
-				</Collapse>
-			</Card>
-		</>
-	);
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <AddToBag text={"Añadir al carrito"} id={id} />
+            </IconButton>
+            <IconButton aria-label="share">
+              <FavoriteIcon />
+            </IconButton>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+        </CardActionArea>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>información adicional</Typography>
+            <Typography paragraph>{description}</Typography>
+            <div>
+              <Button
+                variant="contained"
+                color="primary"
+                Link
+                component={Link}
+                href={`/productDetail/${id}`}
+              >
+                Ver más
+              </Button>
+            </div>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </>
+  );
 };
 
 export default withStyles({
-	title: {},
-	price: {},
+  title: {},
+  price: {},
 })(CardProduct);
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,78 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import productsService from '../../Services/products';
-import { setAllProducts } from '../../Redux/Actions/productsActions';
-import imgHome2 from '../../Assets/Images/imgHome2jpg.jpg';
-import Loading from '../../Components/Loading';
-import Landing from '../../Components/Landing/Landing-page';
-import Footer from '../../Components/Footer';
-import styles from './Home.module.css';
-import { Navbar } from '../../Components/Navbar/Navbar';
-import Filter from '../../Components/Filter';
-import CardsProducts from '../../Components/CardsProducts';
-import ScoreMax from '../../Components/ScoreMax/ScoreMax';
-import { Paginate } from '../../Utils/paginate';
-import { FaArrowCircleRight } from 'react-icons/fa';
-import { FaArrowCircleLeft } from 'react-icons/fa';
-import { Typography, Button } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import productsService from "../../Services/products";
+import { setAllProducts } from "../../Redux/Actions/productsActions";
+import imgHome2 from "../../Assets/Images/imgHome2jpg.jpg";
+import Loading from "../../Components/Loading";
+import Landing from "../../Components/Landing/Landing-page";
+import Footer from "../../Components/Footer";
+import styles from "./Home.module.css";
+import { Navbar } from "../../Components/Navbar/Navbar";
+import Filter from "../../Components/Filter";
+import CardsProducts from "../../Components/CardsProducts";
+import ScoreMax from "../../Components/ScoreMax/ScoreMax";
+import { Paginate } from "../../Utils/paginate";
+import { FaArrowCircleRight } from "react-icons/fa";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { Typography, Button } from "@mui/material";
 
 export const Home = () => {
-	const productsFilter = useSelector(
-		(state) => state.productsReducer.produtsFilter
-	);
-	const dispatch = useDispatch();
+  const productsFilter = useSelector(
+    (state) => state.productsReducer.produtsFilter
+  );
+  const dispatch = useDispatch();
 
-	const [pageNumber, setPageNumber] = useState(0);
-	const elemPage = 8;
-	useEffect(() => {
-		productsService.getAllProducts().then(({ products }) => {
-			dispatch(setAllProducts(products));
-		});
-	}, [dispatch]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const elemPage = 8;
+  useEffect(() => {
+    productsService.getAllProducts().then(({ products }) => {
+      dispatch(setAllProducts(products));
+    });
+  }, [dispatch]);
 
-	if (!productsFilter) {
-		return <Loading />;
-	}
+  if (!productsFilter) {
+    return <Loading />;
+  }
 
-	return (
-		<div className={styles.containerHome}>
-			<Landing />
-			<Navbar />
-			<ScoreMax />
-			{/* <div className={styles.containerInfo3}>
+  return (
+    <div className={styles.containerHome}>
+      <Landing />
+      <Navbar filter={false} />
+      <ScoreMax />
+      {/* <div className={styles.containerInfo3}>
       
        <img src={imgHome2} alt="imagen" width="100%" height="250px" />
       </div> */}
-			<Filter />
-			<div className={styles.containerInfo4}>
-				<div className={styles.nuevo}>
-					<Typography>Nuevo en</Typography>
-				</div>
-				<div>
-					<div className={styles.paginado_home}>
-						<Button
-							className={styles.paginado_btn}
-							onClick={() => {
-								setPageNumber(pageNumber - 1);
-							}}
-						>
-							<FaArrowCircleLeft /> Anterior
-						</Button>
-						<Button
-							className={styles.paginado_btn}
-							onClick={() => {
-								setPageNumber(pageNumber + 1);
-							}}
-						>
-							Siguiente <FaArrowCircleRight />
-						</Button>
-					</div>
-					<CardsProducts
-						products={Paginate(productsFilter, pageNumber, elemPage)}
-					/>
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+      <Filter />
+      <div className={styles.containerInfo4}>
+        <div className={styles.nuevo}>
+          <Typography>Nuevo en</Typography>
+        </div>
+        <div>
+          <div className={styles.paginado_home}>
+            <Button
+              className={styles.paginado_btn}
+              onClick={() => {
+                setPageNumber(pageNumber - 1);
+              }}
+            >
+              <FaArrowCircleLeft /> Anterior
+            </Button>
+            <Button
+              className={styles.paginado_btn}
+              onClick={() => {
+                setPageNumber(pageNumber + 1);
+              }}
+            >
+              Siguiente <FaArrowCircleRight />
+            </Button>
+          </div>
+          <CardsProducts
+            products={Paginate(productsFilter, pageNumber, elemPage)}
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
