b92e930119b405a71709cb473e5c075bfe03e1cf
jonathan4342 <osoriogarrido7@gmail.com>
2022-03-25 23:20:01 -0500

    nuevo commit

--- src/Pages/clientInterface/MisProductos/MiProductos.jsx
+++ src/Pages/clientInterface/MisProductos/MiProductos.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import styles from './MiProductos.module.scss'
 export const MiProductos = () => {
+
+    const { userId } = useSelector(state => state.userReducer)
+    
     return (
+
         <div className={styles.contProduc}>Aqui van todos los productos a mi nombre </div>
     )
 }
