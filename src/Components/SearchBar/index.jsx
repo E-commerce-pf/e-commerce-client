bae2107e229106688b7d8d0d44df6a0ef9719132
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-30 09:42:50 -0500

    Funcionalidad barrra autocomplete

--- src/Components/SearchBar/index.jsx
+++ src/Components/SearchBar/index.jsx
@@ -1,10 +1,13 @@
 import { Autocomplete, TextField } from "@mui/material";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
   const [input, setInput] = useState(null);
   const products = useSelector((state) => state.productsReducer.allProducts);
+  const navigate = useNavigate();
+
   return (
     <Autocomplete
       options={products}
@@ -13,6 +16,11 @@ const SearchBar = () => {
       value={input}
       onChange={(event, newValue) => {
         setInput(newValue);
+        if (newValue) {
+          navigate(`/productDetail/${newValue.id}`);
+        } else {
+          navigate("/");
+        }
       }}
       sx={{ width: 300 }}
       renderInput={(params) => <TextField {...params} label="Buscar" />}
