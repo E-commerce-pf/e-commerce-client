7c00b0c26a2955e4fec61b291f4e54f28fabe35c
carlafaes <carlafaes18@hotmail.com>
2022-03-29 20:06:46 -0600

    mis datos

--- src/Pages/clientInterface/ClientHome.jsx
+++ src/Pages/clientInterface/ClientHome.jsx
@@ -14,6 +14,7 @@ import EditUser from './editUser/EditUser';
 import userService from "../../Services/user";
 import { logoutUser } from "../../Redux/Actions/userActions";
 import Loading from "../../Components/Loading/index";
+import {FaArchive} from 'react-icons/fa';
 
 import { notifySuccess } from "../../Utils/notifications";
 
@@ -104,7 +105,7 @@ export const ClientHome = () => {
         </button>
         <button onClick={openEdit}>
           {" "}
-          <AiOutlineStar className={styles.btn} />
+          <FaArchive className={styles.btn} />
           Mis datos
         </button>
       </div>
