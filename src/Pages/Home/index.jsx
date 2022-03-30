006afeb264e042f6c0d11f7d5a501e0438cbfcab
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-30 18:21:58 -0500

    Const inecesarias en home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import productsService from "../../Services/products";
-import { setAllProducts } from "../../Redux/Actions/productsActions";
-import Loading from "../../Components/Loading";
+import React, { useState } from "react";
 import Landing from "../../Components/Landing/Landing-page";
 import Footer from "../../Components/Footer";
 import styles from "./Home.module.css";
@@ -11,23 +7,7 @@ import ScoreMax from "../../Components/ScoreMax/ScoreMax";
 import ContactForm from "../../Components/ContactForm/ContactForm";
 
 export const Home = () => {
-  const productsFilter = useSelector(
-    (state) => state.productsReducer.produtsFilter
-  );
-
-  const dispatch = useDispatch();
-
   const [toggle, setToggle] = useState(false);
-
-  useEffect(() => {
-    productsService.getAllProducts().then((products) => {
-      dispatch(setAllProducts(products));
-    });
-  }, [dispatch]);
-
-  if (!productsFilter) {
-    return <Loading />;
-  }
 
   return (
     <div className={styles.containerHome}>
