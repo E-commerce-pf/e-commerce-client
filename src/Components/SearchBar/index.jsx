f7f87992eac8ed301aaa4c2bbf38f0eca49ea780
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-31 14:48:58 -0500

    Bug SearchBar y Reducer

--- src/Components/SearchBar/index.jsx
+++ src/Components/SearchBar/index.jsx
@@ -1,6 +1,7 @@
 import { Autocomplete, TextField } from "@mui/material";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+
 import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Landing from "../../Components/Landing/Landing-page";
 import Footer from "../../Components/Footer";
+import Loading from "../../Components/Loading";
+
 import styles from "./Home.module.css";
 import { Navbar } from "../../Components/Navbar/Navbar";
 import ScoreMax from "../../Components/ScoreMax/ScoreMax";
 import ContactForm from "../../Components/ContactForm/ContactForm";
+import { useDispatch, useSelector } from "react-redux";
+
+import productsService from "../../Services/products";
+import { setAllProducts } from "../../Redux/Actions/productsActions";
 
 export const Home = () => {
   const [toggle, setToggle] = useState(false);
+  const products = useSelector((state) => state.productsReducer.allProducts);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    productsService.getAllProducts().then((res) => {
+      dispatch(setAllProducts(res));
+    });
+  }, [dispatch]);
+
+  if (!products) return <Loading />;
 
   return (
     <div className={styles.containerHome}>
