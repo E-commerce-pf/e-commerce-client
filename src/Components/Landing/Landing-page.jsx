9cbd0f831f0cc86669c9642b1d335f9530fc24d1
carlafaes <carlafaes18@hotmail.com>
2022-04-04 18:31:09 -0300

    carrusel infinito landing

--- src/Components/Landing/Landing-page.jsx
+++ src/Components/Landing/Landing-page.jsx
@@ -6,7 +6,12 @@ import IMG3 from "./images/pc.jpeg";
 import IMG4 from "./images/watch.jpeg";
 import "./styles/landingPage.modules.css";
 
+const imagenes = [IMG1,IMG2,IMG3,IMG4]
 const Landing = () => {
+
+  const carouselRef = React.useRef(null);
+  const totalPages= imagenes.length;
+  const resetTimeout= imagenes[0];
   const breakPoints = [
     { width: 100, itemsToShow: 1 },
     { width: 500, itemsToShow: 1 },
@@ -24,14 +29,24 @@ const Landing = () => {
           itemPadding={[10, 10]}
           focusOnSelect={false}
           verticalMode
+          ref={carouselRef}
+          onNextEnd={({ index }) => {
+            clearTimeout(resetTimeout)
+            if (index + 1 === totalPages) {
+                if (carouselRef?.current?.goTo) {
+                    resetTimeout = setTimeout(() => {
+                        if (carouselRef?.current?.goTo) {
+                            carouselRef.current.goTo(0)
+                        }
+                    }, 1000)
+                }
+            }
+        }}
         >
-          <img className="img-carrousel" src={IMG1} alt="carousel1" />
+          {imagenes && imagenes.map((el)=>(
 
-          <img className="img-carrousel" src={IMG2} alt="carousel2" />
-
-          <img className="img-carrousel" src={IMG3} alt="carousel3" />
-
-          <img className="img-carrousel" src={IMG4} alt="carousel4" />
+          <img className="img-carrousel" src={el} alt="carousel1" />
+          ))}
         </Carousel>
       </div>
     </div>
