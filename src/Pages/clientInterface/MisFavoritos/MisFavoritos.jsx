edeb73aff0290b0e18cd3251304406b14b504e29
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2022-04-04 14:45:55 -0500

    Cambio de nombres en card. Mis favoritos

--- src/Pages/clientInterface/MisFavoritos/MisFavoritos.jsx
+++ src/Pages/clientInterface/MisFavoritos/MisFavoritos.jsx
@@ -9,53 +9,61 @@ import { DeleteFavorite } from "../../../Redux/Actions/userActions";
 import styles from "./MisFavoritos.module.scss";
 
 export const MisFavoritos = ({ Favorites, setUser }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const [loading, setLoading] = useState(false)
+  const deleteFav = async (favorito) => {
+    setLoading(true);
+    try {
+      await dispatch(DeleteFavorite(favorito));
 
-    const deleteFav = async (favorito) => {
-        setLoading(true)
-        try {
-            await dispatch(DeleteFavorite(favorito))
-
-            setUser(user => {
-                return {
-                    ...user,
-                    Favorites: user.Favorites.filter(fav => fav.id !== favorito.id)
-                }
-            })
-
-        } catch (error) {
-            toast.error("Algo salió mal")
-        } finally {
-            setLoading(false)
-        }
+      setUser((user) => {
+        return {
+          ...user,
+          Favorites: user.Favorites.filter((fav) => fav.id !== favorito.id),
+        };
+      });
+    } catch (error) {
+      toast.error("Algo salió mal");
+    } finally {
+      setLoading(false);
     }
-    return (
-        <div className={styles.contFav}>
-            {
-                Favorites.map(el => (
-
-                    <div key={el.id} className={styles.contCard}>
-                        <div className={styles.delete}><MdDeleteForever className={styles.svg} onClick={() => deleteFav(el)} /></div>
-                        <div onClick={() => navigate(`/productDetail/${el.Product.id}`)} className={styles.cardInfo}>
-                            <h3>{el.Product.title}</h3>
-                            <img src={el.Product.image} alt="img" width='200px' height='220px' />
-                            <p className={styles.parrafo}>{el.Product.description}</p>
-                            <div className={styles.cardInfo1}>
-                                <span>Precio: <MdOutlineAttachMoney />{el.Product.price}. </span>
-                                <span>Descuento:{el.Product.discount * 100} %.</span>
-                                <span>Stock: {el.Product.stock} uds.</span>
-                            </div>
-                        </div>
-                    </div>
-
-                ))
-            }
-            {
-                loading && <OpacityLoader />
-            }
+  };
+  return (
+    <div className={styles.contFav}>
+      {Favorites.map((el) => (
+        <div key={el.id} className={styles.contCard}>
+          <div className={styles.delete}>
+            <MdDeleteForever
+              className={styles.svg}
+              onClick={() => deleteFav(el)}
+            />
+          </div>
+          <div
+            onClick={() => navigate(`/productDetail/${el.Product.id}`)}
+            className={styles.cardInfo}
+          >
+            <h3>{el.Product.title}</h3>
+            <img
+              src={el.Product.image}
+              alt="img"
+              width="200px"
+              height="220px"
+            />
+            <p className={styles.parrafo}>{el.Product.description}</p>
+            <div className={styles.cardInfo1}>
+              <span>
+                Price: <MdOutlineAttachMoney />
+                {el.Product.price}.{" "}
+              </span>
+              <span>Discount:{el.Product.discount * 100} %.</span>
+              <span>Stock: {el.Product.stock}</span>
+            </div>
+          </div>
         </div>
-    )
+      ))}
+      {loading && <OpacityLoader />}
+    </div>
+  );
 };
