bea82427877134aeee623ac154323029c5f37d99
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2022-04-05 16:21:29 -0500

    Fix add to cart in home

--- src/Components/AddToBag/index.jsx
+++ src/Components/AddToBag/index.jsx
@@ -1,31 +1,37 @@
-import React from 'react';
-import { useDispatch,useSelector } from 'react-redux';
-import { addProductToBag, newCartChange } from '../../Redux/Actions/productsActions';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { addToLocalStorageIds, addProductToCartDb } from '../../Utils/shoppingBag';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addProductToBag,
+  newCartChange,
+} from "../../Redux/Actions/productsActions";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import {
+  addToLocalStorageIds,
+  addProductToCartDb,
+} from "../../Utils/shoppingBag";
 import { notifyError } from "../../Utils/notifications";
 
-const AddToBag = ({ text, id, user,bagProducts}) => {
-	const dispatch = useDispatch();
-	const stockSucess=(product)=>{
-		return product.amount+1<=product.stock;
-	}
-	const addToCart = (id) => {
-		dispatch(newCartChange(true));
-		let product=bagProducts.find(p=>p.id===id);
-		if(!product||stockSucess(product)){
-			dispatch(addProductToBag(id));
-			addToLocalStorageIds(id);	
-		} else {
-			notifyError(`Maximum stock reached ${product.amount}`)
-		}
-	};
+const AddToBag = ({ text, id, user, bagProducts }) => {
+  const dispatch = useDispatch();
+  const stockSucess = (product) => {
+    return product.amount + 1 <= product.stock;
+  };
+  const addToCart = (id) => {
+    dispatch(newCartChange(true));
+    let product = bagProducts?.find((p) => p.id === id);
+    if (!product || stockSucess(product)) {
+      dispatch(addProductToBag(id));
+      addToLocalStorageIds(id);
+    } else {
+      notifyError(`Maximum stock reached ${product.amount}`);
+    }
+  };
 
-	return (
-		<AddShoppingCartIcon onClick={() => addToCart(id)}>
-			{text}
-		</AddShoppingCartIcon>
-	);
+  return (
+    <AddShoppingCartIcon onClick={() => addToCart(id)}>
+      {text}
+    </AddShoppingCartIcon>
+  );
 };
 
 export default AddToBag;
