11becc1bd097bbff119b32fc06aeb10ba681d050
Dairo01001 <garcianaranjodairo@gmail.com>
2022-04-05 19:47:43 -0500

    Template

--- src/Templates/newletterSubscribe.js
+++ src/Templates/newletterSubscribe.js
@@ -2,13 +2,105 @@ const baseUri = "http://localhost:3000";
 
 export const subscribe = (id) => {
   return `    
-     ${baseUri}/api/subscribe/${id}
-    `;
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Subscribe</title>
+  
+      <!-- Styles -->
+      <link rel="stylesheet" />
+      <style>
+        * {
+          box-sizing: border-box;
+        }
+        main {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          max-width: 600px;
+          margin: 0 auto;
+          padding: 0 15px;
+        }
+        h1 {
+          text-align: center;
+        }
+        img {
+          transform: scale(0.3);
+        }
+        div {
+          width: 100%;
+          align-items: left;
+        }
+      </style>
+    </head>
+    <body>
+      <main>
+        <h1>Hello 👋🏼</h1>
+        <p>
+          You have Subscribe to our newsletter to get the latest news and updates.
+        </p>
+        <p>
+          <a href="${baseUri}/unsubscribe/${id}">Unsubscribe</a>
+        </p>
+        <div>
+          <img src="https://i.ibb.co/b5NkjVL/Everylogopf.png" alt="Everylogopf" />
+        </div>
+      </main>
+    </body>
+  </html>  
+  `;
 };
 
-export const unsubscribe = (id) => {
+export const unsubscribe = () => {
   return `
-            
-    
-    `;
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Subscribe</title>
+  
+      <!-- Styles -->
+      <link rel="stylesheet" />
+      <style>
+        * {
+          box-sizing: border-box;
+        }
+        main {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          max-width: 600px;
+          margin: 0 auto;
+          padding: 0 15px;
+        }
+        h1 {
+          text-align: center;
+        }
+        img {
+          transform: scale(0.3);
+        }
+        div {
+          width: 100%;
+          align-items: left;
+        }
+      </style>
+    </head>
+    <body>
+      <main>
+        <h1>Hello 👋🏼</h1>
+        <p>
+          You have Unsubscribe to our newsletter!.
+        </p>
+        <div>
+          <img src="https://i.ibb.co/b5NkjVL/Everylogopf.png" alt="Everylogopf" />
+        </div>
+      </main>
+    </body>
+  </html>     
+  `;
 };
